3146e8d3212f783cd81ef79b0a6d97b4b1b07e2a
chall2022 <cchall5@crimson.ua.edu>
2024-09-10 13:15:48 -0500

    initial commit of project files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Fall2024_Assignment1_cchall5.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
